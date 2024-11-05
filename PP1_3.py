aafd0836e239ddba4e9ec4612440158a5441b588
Benson-Zheng-ICD2O1-02 <benson.zheng3@student.tdsb.on.ca>
2024-11-05 14:23:50 -0500

    Update PP1_3.py
    eer

--- PP1_3.py
+++ PP1_3.py
@@ -4,8 +4,9 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n* * * * *\n*       *\n*       *\n*       *\n* * * * *\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
- 
+  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *)
+  print("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n")
+  print("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
 def q2():
   #Write Assignment code here
     word1 = "Hello "
